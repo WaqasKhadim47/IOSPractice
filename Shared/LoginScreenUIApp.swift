4c4e9abfce8d432c18e98a33d7d6ba76715a7b6f
waqas.khadim <waqas.khadim@invozone.com>
2022-08-16 17:13:17 +0500

    Initial Commit

---
+++ Shared/LoginScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenUIApp.swift
+//  Shared
+//
+//  Created by Waqas Khadim on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
